f3eebc0f5761e2772c6d4ceb6c1f0f1c62a77dc3
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-15 15:33:34 +0600

    component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,41 @@ import './App.css';
 
 function App() {
   
+  return(
+    <>
+      {/* use component  */}
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h1>Hey, This is an amazing morning</h1>
+      <Nayok></Nayok>
+      <Nayok></Nayok>
+      <Nayok></Nayok>
+      <Nayok></Nayok>
+    </>
+  )
+  
+}
+
+// create component 
+function Person(){
+  return (
+  <>
+  <h1>Mahfuzur Rahman</h1>
+  <h2>Singer</h2>
+  <p>Album: 120+</p>
+  </>
+  )
+}
+
+function Nayok(){
+  return(
+    <>
+      <h1>Movie: 100% Love</h1>
+      <p>Main Actor: Zeet</p>
+      <p>age: 50</p>
+    </>
+  )
 }
 
 export default App;
